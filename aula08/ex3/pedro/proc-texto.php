c95d25e10fe0ee66ddc1d4e6ce6c51bb1661c09f
pedroapa <111130865+pedroapa@users.noreply.github.com>
2022-08-23 11:36:25 -0300

    Fazendo ex3 da aula08
    Agora vamos configurar o CSS no php. Eu tentei fazer sozinho, mas não sei como mexer em um usando o outro. Agora estou  fazendo do jeito do professor para aprender e tentar fazer o meu.

---
+++ aula08/ex3/pedro/proc-texto.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title>Criando o texto personalizado</title>
+    
+</head>
+<body>
+    <div>
+        <?php 
+            $titulo = $_GET["titulo"];
+            $fonte = $_GET["fonte"];
+            echo "<h1>$titulo</h1>";
+
+
+            
+        ?>
+        <a href="conf-texto.html" target="_self" rel="prev"><br> Voltar</a>
+    </div>
+    
+</body>
+</html>
