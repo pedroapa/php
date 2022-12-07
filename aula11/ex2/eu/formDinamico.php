b3f48bdec2cffb608a70c1546f4ec44bd01c2d23
pedroapa <111130865+pedroapa@users.noreply.github.com>
2022-12-07 10:44:56 -0300

    Quebrando a cabeça
    Nesta atividade preciso dentro do formulário criar 5 inputs, mas não to sabendo como fazer isso. Pensei que rodava de boa os comando html no php, mas parece que só da certo se estiver dentro do echo. Pensei assim, no código html eu iria criar o formulário e dentro dele usar o php que vai criar um input de texto, mas vai fazer esse processo 5 vezes.

---
+++ aula11/ex2/eu/formDinamico.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../_css/estilo.css">
+    <title>Formulário Criado por Repetição</title>
+</head>
+<body>
+    <div>
+
+        <form action="" method="get">
+            <?php
+                $conta = 5;
+
+                while( $conta <= 5 ){
+                    echo "<input type="text" name="$conta">";
+
+                }
+            ?>
+        </form>
+        
+    </div>
+    
+</body>
+</html>
