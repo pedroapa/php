ea3ad4b73f5be3b2ce3bdaf0b2f5713475b5f3b1
pedroapa <111130865+pedroapa@users.noreply.github.com>
2022-12-08 09:29:58 -0300

    Segunda parte da Resposta professor
    Coloquei o complemento  da resolução do professor, onde ele pega os valores do input numéricos e mostra na tela usando variáveis de variáveis.
    Ele em cada interação do while cria um nome que vai ser usado para nomear uma variavel que vai guardar o parâmetro de um input numérico, isso é repetido 5 vezes.  Outro while imprime os valores das variáveis na tela, podendo ser de um jeito simples, onde alguem poe no echo que apareça os valores das variaveis: num1, num2, num3 etc. Enquanto a forma mais otimizada é usando laço, que em cada interação um nome é criado que vai renomear um variável, , o mesmo de antes, e por fim ela é escrita na tela

--- aula11/ex2/prof/02parte1.php
+++ aula11/ex2/prof/02parte1.php
@@ -14,13 +14,12 @@
                 $c = 1;
 
                 while( $c <= 5 ){
-                    echo "Valor $c: <input type='number' name='v$c' max='100' min='0' value='$c'> <br>";
+                    echo "Valor $c: <input type='number' name='v$c' max='100' min='0' value='0'> <br>";
                     $c++;
                 }
             ?>
             <input type="submit" value="Enviar">
         </form>
     </div>
-    
 </body>
 </html>
