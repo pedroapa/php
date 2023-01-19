61120f51086c3a3599df685b68dd397e8430f247
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-01-19 11:50:01 -0300

    Encontrei bug, mas apareceu outro
    Só escrevi insert tudo em maiúsculo para o negocio entender que era um comando, que estranho... Doido que no you tube cara disse para por em maiúsculo mesmo.
    Depois notei que não estava usando a função mysqli_query() do php kkk assim nunca ia mesmo.
    Mas depois disso apareceu um problema novo. Agora, escolho uma data de nascimento, chego no php certinho, da para ver usando echo, mas no banco fica salvo 1983. Tipo nem faz sentido, quando tentei cadastrar de novo, foi para 1984. Não sei o que esta acontecendo..., estou pensando que pode ter aver que tenho dois código php diferente que mexe na mesma tabela. Não sei se isso afeta em alguma coisa.

--- Acesso-Banco/8/cadastrar.php
+++ Acesso-Banco/8/cadastrar.php
@@ -14,10 +14,13 @@
         $vCpf = isset($_POST["tCpf"])? $_POST["tCpf"]: "111-11-1111";
         $vDt_nasc = isset($_POST["tNasc"])? $_POST["tNasc"]: "2000-01-01";
 
-        $sql = "insert into usuario 
+        echo "Nome: $vNome <br>CPF: $vCpf <br>Data Nasc: $vDt_nasc <br><br>";
+
+        $sql = "INSERT INTO usuario 
         (id, nome, cpf, dt_nascimento)
         values
         (DEFAULT, '$vNome', '$vCpf', $vDt_nasc)";
+        $res = mysqli_query($conn, $sql);
 
         if(mysqli_affected_rows($conn)){
             echo "Cadastro realizado com sucesso.";
