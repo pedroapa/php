5816261c2ce6428e2cb3cd55257fb8069ef17ace
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-01-19 13:59:35 -0300

    Concertando
    Eu tinha colocado  dentro da tabela um formulário e isso estava me deixando confuso, era isso, deixei a tabela dentro do formulário e agora os botões ficaram em baixo no lugar certo.

--- Acesso-Banco/8/exclusao.php
+++ Acesso-Banco/8/exclusao.php
@@ -9,34 +9,44 @@
 <body>
     <main>
         <h1>Selecione um Registro para ser Deletado</h1>
-        <table>
-            <thead>
-                <th>Nome</th>
-                <th>CPF</th>
-                <th>Data Nascimento</th>
-                <th>Selecionar</th>
-            </thead>
+        
 
-            <?php
-                include "conexao.php";
+            <form action="excluir.php" method="post">
+                <table>
+                    <thead>
+                        <th>Nome</th>
+                        <th>CPF</th>
+                        <th>Data Nascimento</th>
+                        <th>Selecionar</th>
+                    </thead>
+                    <?php
+                        include "conexao.php";
 
-                $sql = "SELECT id, nome, cpf, dt_nascimento FROM usuario";
-                $res = mysqli_query($conn, $sql);
+                        $sql = "SELECT id, nome, cpf, dt_nascimento FROM usuario";
+                        $res = mysqli_query($conn, $sql);
 
-                while( $reg = mysqli_fetch_row($res) ){
-                    $id = $reg[0];
-                    $vNome = $reg[1];
-                    $vCpf = $reg[2];
-                    $vNasc = $reg[3];
+                        while( $reg = mysqli_fetch_row($res) ){
+                            $id = $reg[0];
+                            $vNome = $reg[1];
+                            $vCpf = $reg[2];
+                            $vNasc = $reg[3];
 
-                    echo "<tr>";
-                    echo "<td>$vNome</td><td>$vCpf</td><td>$vNasc</td>";
-                    echo "<td><input type='radio' name='sel' value='$id'></td>";
-                    echo "</tr>";
+                            echo "<tr>";
+                            echo "<td>$vNome</td><td>$vCpf</td><td>$vNasc</td>";
+                            echo "<td><input type='radio' name='sel' value='$id'></td>";
+                            echo "</tr>";
 
-                }
-            ?>
-        </table>
+                        }
+                    ?>
+                </table>
+                
+                <br>
+                <input type="submit" value="Excluir">
+                <input type="reset" value="Limpar">
+            </form>
+
+            
+        
     </main>
     
 </body>
