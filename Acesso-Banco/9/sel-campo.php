1fe28216eb1a4b90869da20c32f3850de812b1bc
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-01-20 13:53:28 -0300

    Consegui apresentar
    Estava com duvida em como mostrar na tela a parte onde a pessoa escolhia qual campo que queria editar, eu mudei de ideia, quando a pessoa pedir para editar, ela vai mudar todos, e aqueles que não tiver feito nenhuma mudança, vai subscrever o que ja estava escrito. EX:
    O nome Pedro ta cadastrado, quando for editar, se a pessoa não mudar o nome Pedro, na hora de atualizar o banco, vou salvar Pedro de novo.
    
    Agora ta dando um problema, os botões aparecem antes da tabela. Printei a tela para exemplificar.

--- Acesso-Banco/9/sel-campo.php
+++ Acesso-Banco/9/sel-campo.php
@@ -5,6 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Selecionando Campo</title>
+    <style>
+        body {
+            text-align: center;
+        }
+
+        
+        table {
+            text-align: left;
+            margin: auto;
+        }
+
+        thead {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <form action="editar.php" method="post">
@@ -14,9 +29,29 @@
                 <th>CPF</th>
                 <th>Data Nascimento</th>
             </thead>
+            <?php
+                include "conexao.php";
 
-            
+                $vId = isset($_POST["sel"])? $_POST["sel"]: '0';
+                
+                $sql = "SELECT * FROM usuario
+                WHERE id = $vId";
+                $res = mysqli_query($conn, $sql);
 
+                $reg = mysqli_fetch_row($res);
+                $vNome = $reg[1];
+                $vCpf = $reg[2];
+                $vNasc = $reg[3];
+                
+                echo "<tr>";
+                echo "<td><input type='text' name='tNom' id='cNom' value='$vNome'></td>";
+                echo "<td><input type='text' name='tCpf' id='cCpf' value='$vCpf'></td>";
+                echo "<td><input type='date' name='tNasc' id='cNasc' value='$vNasc'></td>";
+                echo "</tr>";
+            ?>
+            <br>
+            <input type="submit" value="Salvar">
+            <a href="sel-reg.php" target="_self" rel="prev">Cancelar</a>
         </table>
     </form>
     
